c1a16cddcccf63692fd6a4adf1bfa0ca9f6bfd4d
vtnboolean <vnthuanx@gmail.com>
2022-09-22 17:38:38 +0200

    posts user_id foreign key added

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\User;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 
@@ -13,8 +14,13 @@ class PostsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i=0; $i < 50; $i++) {
+
+        $users = User::all();
+
+
+        for ($i=0; $i < 100; $i++) {
             $newPost = new Post();
+            $newPost->user_id = $faker->randomElement($users)->id;
             $newPost->title = $faker->realtext(35);
             $newPost->author = $faker->userName();
             $newPost->post_content = $faker->paragraphs(5, true);
