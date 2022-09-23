2a1f48428ea8883eefd4b2a1d7b076915e122b41
vtnboolean <vnthuanx@gmail.com>
2022-09-23 18:08:10 +0200

    categories seeder added

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class CategoriesTableSeeder extends Seeder
 {
@@ -9,8 +11,27 @@ class CategoriesTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $categoryNames = [
+            'sport',
+            'news',
+            'Anime',
+            'cybersecurity',
+            'networking',
+            'laravel',
+            'coding',
+            'movies',
+            'TVSeries',
+            'books',
+        ];
+
+        foreach ($categoryNames as $categoryName){
+            $category = new Category;
+            $category->name = $categoryName;
+            $category->color = $faker->hexColor();
+            $category->slug = Str::slug($categoryName, '-');
+            $category->save();
+        }
     }
 }
--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use App\User;
 use Illuminate\Database\Seeder;
@@ -16,11 +17,13 @@ class PostsTableSeeder extends Seeder
     {
 
         $users = User::all();
+        $categories = Category::all();
 
 
         for ($i=0; $i < 100; $i++) {
             $newPost = new Post();
             $newPost->user_id = $faker->randomElement($users)->id;
+            $newPost->category_id = $faker->randomElement($categories)->id;
             $newPost->title = $faker->realtext(35);
             $newPost->post_content = $faker->paragraphs(5, true);
             $newPost->post_image = $faker->imageUrl();
